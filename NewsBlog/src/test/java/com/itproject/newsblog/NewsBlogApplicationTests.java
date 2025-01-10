9bd6b5e778cc35ce91129afd7b178153369b604e
Kostadin Todorov <kostadintodorov_kt@abv.bg>
2025-01-11 00:34:34 +0200

    Creating Spring Boot project

---
+++ NewsBlog/src/test/java/com/itproject/newsblog/NewsBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itproject.newsblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
