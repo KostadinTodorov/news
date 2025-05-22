0eba4ef13b32f76aa8febddde4b06ab282c2019f
Kostadin Todorov <kostadintodorov_kt@abv.bg>
2025-05-22 18:13:58 +0300

    Adding more info, regarding the project DB and API's endpoints.

--- NewsBlog/src/main/java/bg/tuvarna/sit/newsblog/security/JwtAuthenticationFilter.java
+++ NewsBlog/src/main/java/bg/tuvarna/sit/newsblog/security/JwtAuthenticationFilter.java
@@ -8,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -20,7 +19,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtTokenProvider jwtTokenProvider;
-    private final UserDetailsService userDetailsService;
+    private final CustomUserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
